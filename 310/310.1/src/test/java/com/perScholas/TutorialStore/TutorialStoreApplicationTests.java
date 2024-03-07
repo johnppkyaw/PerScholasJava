84a32fdc3767d0e58fcbd1aa958a8af01493e101
John Kyaw <jjpyae@gmail.com>
2024-03-07 13:33:17 -0800

    GL 310.1.1 complete

---
+++ 310/310.1/src/test/java/com/perScholas/TutorialStore/TutorialStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perScholas.TutorialStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
