57ae11a74dc8bee3fbc5decd42579e659560a575
John Kyaw <jjpyae@gmail.com>
2024-03-08 09:35:38 -0800

    GL 310.1.2 complete

---
+++ 310/310.1.2soapdemo/src/test/java/dev/johnkyaw/soapdemo/SoapdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.johnkyaw.soapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
