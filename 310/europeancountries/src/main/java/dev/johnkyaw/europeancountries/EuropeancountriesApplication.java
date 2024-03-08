062e921d78138815ac311192dbc1edb255f27edc
John Kyaw <jjpyae@gmail.com>
2024-03-08 10:07:17 -0800

    complete europeancountries assignment (310.1.3)

---
+++ 310/europeancountries/src/main/java/dev/johnkyaw/europeancountries/EuropeancountriesApplication.java
@@ -0,0 +1,13 @@
+package dev.johnkyaw.europeancountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuropeancountriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuropeancountriesApplication.class, args);
+	}
+
+}
